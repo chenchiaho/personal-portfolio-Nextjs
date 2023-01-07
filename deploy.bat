933e22abc3c736dc55c7459b0f09e7df82afb3e4
chenchiaho <chenchiaho@gmail.com>
2023-01-07 20:41:08 +0800

    Deploy to GitHub Pages

--- deploy.bat
+++ deploy.bat
@@ -5,6 +5,6 @@ cd out
 git init
 git add -A
 git commit -m "Deploy to GitHub Pages"
-git push -f git@github.com:chenchiaho/react-personal-portfolio.git master:gh-pages
+git push -f git@github.com:chenchiaho/react-personal-portfolio.git main:gh-pages
 
 cd ..
